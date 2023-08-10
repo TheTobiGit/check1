37c6064066ed1c409737161dadf1f49ca749b7cb
Gideon Sarfo <aemonsarfo@outlook.com>
2023-08-10 02:48:35 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand1: '#2A5198',
-        brand2: '#FDB515',
-        brand3: '#00AEEF',
-      },
-      backgroundImage: {
-        'hero-image': "url('./src/assets/hero.jpg')",
-      },
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      brand1: '#2A5198',
+      brand2: '#FDB515',
+      brand3: '#00AEEF',
+    },
+    backgroundImage: {
+      'hero-image': "url('./src/assets/hero.jpg')",
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
